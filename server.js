f06d26f82ad5a7e38c988129c376b013b869883e
Max Silbiger <max.silbiger@gmail.com>
2016-08-28 18:22:02 -0400

    Generates short urls, successfully redirects to original urls

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongo = require('mongodb').MongoClient;
 var mongoUri = process.env.MONGO_URI
 
 var shortid = require('shortid');
+var valid_url = require('valid-url');
 
 var port = process.env.PORT || 8080;
 var webhost = "http://localhost:8080/";
@@ -13,24 +14,59 @@ var webhost = "http://localhost:8080/";
 // (*) prevents the additional slashes of a url from confusing Express.
 app.get('/new/:url(*)', function(req, res, next) {
 	var longUrl = req.params.url;
-	
-	mongo.connect(mongoUri, function(err, db) {
+
+	if (valid_url.isUri(longUrl)) {
+		mongo.connect(mongoUri, function(err, db) {
+		// (If you're getting "Parameter 'url' must be string, not undefined" make sure the env variables are loaded)
 		if (err) throw err;
 
 		var collection = db.collection('urls');
 
+		// TODO: Overwrite or don't write a new link with the same original_url.
+
 		var newLink = function(db, callback) {
-			var insertLink = { original_url: longUrl, short_url: "test" };
+			var shortUrl = webhost + shortid.generate();
+			var insertLink = { original_url: longUrl, short_url: shortUrl };
 			collection.insert([insertLink]);
-			res.send(longUrl);
+			res.send(insertLink);
 		};
 
 		newLink(db, function() {
 			db.close();
 		});
 	})
+	} else {
+		res.send({'error': 'Invalid url format.'});
+	}
+})
+
+app.get('/:url(*)', function(req, res, next) {
+	var shortUrlQuery = webhost + req.params.url;
+	console.log(shortUrlQuery);
+
+	if (valid_url.isUri(shortUrlQuery)) {
+		mongo.connect(mongoUri, function(err, db) {
+			if (err) throw err;
+
+			var collection = db.collection('urls');
+			console.log(shortUrlQuery);
+			var result = collection.find({
+				short_url: shortUrlQuery
+			}).toArray(function(err, documents) {
+				if (err) return null;
+				else {
+					console.log(documents);
+					console.log(documents[0]);
+					console.log(documents[0].original_url);
+					var redirectUrl = documents[0].original_url;
+					res.redirect(redirectUrl);
+				}
+			})
+		db.close()
+		})
+	}
 })
 
 app.listen(port, function() {
-	console.log('Express app listening on port' + port);
+	console.log('Express app listening on port ' + port);
 })
